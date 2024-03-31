d7eb60e3033a69381f883bec0c5e91f47a383bff
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-03-31 19:07:03 -0400

    Time: 690 ms (78.06%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,6 +1,4 @@
 # Write your MySQL query statement below
-SELECT distinct v1.author_id as id
-FROM views v1
-WHERE v1.author_id in (select viewer_id from views
-where article_id = v1.article_id)
+Select  distinct author_id as id  from Views
+where author_id = viewer_id 
 order by id
