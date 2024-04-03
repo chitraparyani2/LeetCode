d1d9b86ab6d131d541ff7d45597f3826b674288e
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-04-03 16:30:08 -0400

    Time: 20 ms (33.52%) | Memory: 45.5 MB (45.62%) - LeetSync

--- 829-subdomain-visit-count/subdomain-visit-count.java
+++ 829-subdomain-visit-count/subdomain-visit-count.java
@@ -14,17 +14,21 @@ class Solution {
             String s = strArray[1];
 
             String[] temp = s.split("\\.");
-            String ans = "";
+            StringBuilder sb = new StringBuilder();
 
             for(int i=temp.length-1; i>=0; i--) {
 
                 if(i != temp.length-1) {
-                    ans = temp[i] + "." + ans;
+                    sb.reverse();
+                    sb.append(".");
+                    sb.append(new StringBuilder(temp[i]).reverse());
+                    sb.reverse();
                 } else {
-                    ans = temp[i];
+                    sb.append(temp[i]);
                 }
-                System.out.println("ans " + ans + " " + count);
-              map.put(ans, map.getOrDefault(ans, 0) + count);
+
+               // System.out.println("ans " + ans + " " + count);
+                map.put(sb.toString(), map.getOrDefault(sb.toString(), 0) + count);
 
             //   if(!map.containsKey(ans)) {
             //     map.put(ans, count);
