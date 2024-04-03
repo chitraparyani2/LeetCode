1d58f44dc63eac61b695f99da09fdd086b04d702
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-04-03 14:02:27 -0400

    Time: 6 ms (48.73%) | Memory: 45.8 MB (16.02%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,50 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        int[] indegree = new int[numCourses];
+
+        for(int i=0; i<numCourses; i++) {
+            map.put(i, new ArrayList<>());
+        }
+
+        for(int[] prereq : prerequisites) {
+
+            int p = prereq[0];
+            int q = prereq[1];
+
+            map.get(q).add(p);
+            indegree[p]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i=0; i<indegree.length; i++) {
+            if(indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        while(!queue.isEmpty()) {
+
+            int course = queue.poll();
+
+            for(int c : map.get(course)) {
+                indegree[c]--;
+
+                if(indegree[c] == 0) {
+                    queue.add(c);
+                }
+            }
+        }
+
+        for(int i=0; i<indegree.length; i++) {
+            if(indegree[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
