413e518e23165a9e61ce01f0c24414086d6739ba
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-04-03 15:32:13 -0400

    Time: 4 ms (91.23%) | Memory: 45.7 MB (52.17%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,10 +1,12 @@
 class Solution {
+    int index=0;
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         
         Map<Integer, List<Integer>> map = new HashMap<>();
 
-        int[] indegree = new int[numCourses];
-        int[] result = new int[numCourses];
+        boolean[] visited = new boolean[numCourses];
+        boolean[] inStack = new boolean[numCourses];
+        int[] res = new int[numCourses];
 
         for(int i=0; i<numCourses; i++) {
             map.put(i, new ArrayList<>());
@@ -12,43 +14,45 @@ class Solution {
 
         for(int[] pre : prerequisites) {
 
-            int p = pre[0];
-            int q = pre[1];
-
-            map.get(q).add(p);
-            indegree[p]++;
+            int subject = pre[0];
+            int depSubj = pre[1];
+            
+            map.get(subject).add(depSubj);
         }
 
-        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0; i<numCourses; i++) {
 
-        for(int i=0; i<indegree.length; i++) {
-            if(indegree[i] == 0) {
-                queue.add(i);
-            }
-        }
-
-        int index=0;
-
-        while(!queue.isEmpty()) {
-
-            int element = queue.poll();
-            result[index++] = element;
-
-            for(int e : map.get(element)) {
-                indegree[e]--;
-
-                if(indegree[e] == 0) {
-                    queue.add(e);
-                }
-            }
-        }
-
-        for(int i=0; i<indegree.length; i++) {
-            if(indegree[i] != 0) {
+            boolean r = helper(i, map, visited, inStack, res);
+            if(!r) {
                 return new int[]{};
             }
         }
-
-      return result;  
+       return res; 
     }
+
+    public boolean helper(int course, Map<Integer, List<Integer>> map,
+            boolean[] visited, boolean[] inStack, int[] res) {
+
+                if(inStack[course]) {
+                    return false;
+                }
+
+                if(visited[course]) {
+                    return true;
+                }
+
+                visited[course] = true;
+                inStack[course] = true;
+
+                for(int c : map.get(course)) {
+                    if(!helper(c, map, visited, inStack, res)) {
+                        return false;
+                    }
+                }
+
+                inStack[course] = false;
+                res[index++] = course;
+
+            return true;
+        }
 }
