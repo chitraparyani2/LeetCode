013edc61b1872964d74175373eaa730f7f59b603
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-04-11 14:14:00 -0400

    Time: 80 ms (14.94%) | Memory: 64 MB (13.78%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,50 +1,26 @@
 class Solution {
-    /*
-1,3,-1,-3,5,3,6,7
-
- 5, 3, 6 -> decreasing montonic queue
-
-
-        85
-
- 97,85
-
-
-[1,3,1,2,0,5]
-
-1, 2, 3
-3, 1, 2
-3, 3
-
- 7
-    */
     public int[] maxSlidingWindow(int[] nums, int k) {
-
-        Deque<Integer> dq = new LinkedList<>();
-
-        int []result = new int[nums.length - k + 1];
-
-        int j =0;
-
-        for(int i=0; i<nums.length; i++){
-
-            // maintaining the window of size k
-            while(!dq.isEmpty() && i - dq.getFirst() >= k){
-                dq.removeFirst();
-            }
-
-            // maintaining decreasing monotonic queue
-            // if larger no is coming I am removing all lesser numbers
-            while(!dq.isEmpty() && nums[dq.getLast()] <= nums[i]){
-                dq.removeLast();
-            }
-
-            dq.addLast(i);
-            if(i >= k -1)
-                result[j++] = nums[dq.getFirst()];
-
-        }
-        return result;
         
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) ->
+        nums[b] - nums[a]);
+
+        int size = nums.length-k+1;
+        int index=0;
+        int[] res = new int[size];
+
+        for(int i=0; i<k; i++) {
+            pq.add(i);
+        }
+
+        res[index++] = nums[pq.peek()];
+
+        for(int i=k; i<nums.length; i++) {
+            while(!pq.isEmpty() && i-pq.peek() >= k) {
+                pq.poll();
+            }
+                pq.offer(i);
+                res[index++] = nums[pq.peek()];
+        }
+      return res;  
     }
 }
