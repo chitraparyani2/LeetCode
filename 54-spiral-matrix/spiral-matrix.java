9f19422c64c032228cee8304b3461e26d25b7f6b
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-04-12 17:54:11 -0400

    Time: 0 ms (100.00%) | Memory: 41.7 MB (7.11%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,47 +1,40 @@
 class Solution {
-    /**
-        00 -> 01 -> 02
-        10 -> 11 -> 12
-        20 -> 21 -> 22
-     */
-
     public List<Integer> spiralOrder(int[][] matrix) {
-     
-     List<Integer> result = new ArrayList<>();
-
-     int m = matrix.length;
-     int n = matrix[0].length;
-     int count = m*n;
-     int val = 0;
-
-     int rs=0, cs=0, re=m, ce=n;
-
-    while(rs < re && cs < ce) {
-
-     for(int i=cs; i<ce && val < count; i++) {
-        result.add(matrix[rs][i]);
-        val++;
-     }
-
-     for(int j=rs+1; j<re && val < count; j++) {
-        result.add(matrix[j][ce-1]);
-        val++;
-     }
-     
-     for(int k=ce-2; k>=cs && val < count; k--) {
-        result.add(matrix[re-1][k]);
-        val++;
-     }
-
-     for(int l=re-2; l>rs && val < count; l--) {
-        result.add(matrix[l][rs]);
-        val++;
-     }
-
-        rs++; re--; cs++; ce--;
-    }
         
-        return result;  
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int rs=0, re=m, cs=0, ce=n;
+        int ct=0;
+        int t=m*n;
+
+        List<Integer> res = new ArrayList<>();
+
+        while(rs < re && cs < ce) {
+
+            for(int c=cs; c<ce && ct < t; c++) {
+                res.add(matrix[rs][c]);
+                ct++;
+            }
+
+            for(int r=rs+1; r<re && ct < t; r++) {
+                res.add(matrix[r][ce-1]);
+                ct++;
+            }
+
+            for(int c=ce-2; c>=cs && ct < t; c--) {
+                res.add(matrix[re-1][c]);
+                ct++;
+            }
+
+            for(int r=re-2; r>rs && ct < t; r--) {
+                res.add(matrix[r][cs]);
+                ct++;
+            }
+
+            rs++; re--; cs++; ce--;
+        }
+
+       return res; 
     }
-  
 }
