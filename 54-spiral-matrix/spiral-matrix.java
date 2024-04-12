465a68f3cc008a8501314b7b9d2b23a901c6af95
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-04-12 18:09:09 -0400

    Time: 0 ms (100.00%) | Memory: 41.5 MB (36.40%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -5,31 +5,31 @@ class Solution {
         int n = matrix[0].length;
 
         int rs=0, re=m, cs=0, ce=n;
-        int ct=0;
-        int t=m*n;
+        int count=0;
+        int total=m*n;
 
         List<Integer> res = new ArrayList<>();
 
         while(rs < re && cs < ce) {
 
-            for(int c=cs; c<ce && ct < t; c++) {
+            for(int c=cs; c<ce; c++) {
                 res.add(matrix[rs][c]);
-                ct++;
             }
 
-            for(int r=rs+1; r<re && ct < t; r++) {
+            for(int r=rs+1; r<re; r++) {
                 res.add(matrix[r][ce-1]);
-                ct++;
             }
 
-            for(int c=ce-2; c>=cs && ct < t; c--) {
+                if(rs != re-1) {
+            for(int c=ce-2; c>=cs; c--) {    
                 res.add(matrix[re-1][c]);
-                ct++;
-            }
+                }
+             }
 
-            for(int r=re-2; r>rs && ct < t; r--) {
+             if(cs != ce-1) {
+            for(int r=re-2; r>rs; r--) {
                 res.add(matrix[r][cs]);
-                ct++;
+                }
             }
 
             rs++; re--; cs++; ce--;
