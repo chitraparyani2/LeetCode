966b33f43008a686bd1282a82b99e0eac27c6d5a
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-04-15 12:13:46 -0400

    Time: 17 ms (5.07%) | Memory: 43 MB (8.98%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -7,7 +7,7 @@ class Solution {
 
         m = grid.length;
         n = grid[0].length;
-        int res =0;
+        int res =0, count_fresh=0;
         boolean flag=false;
 
         for(int i=0; i<m; i++) {
@@ -15,9 +15,13 @@ class Solution {
 
                 if(grid[i][j] == 2) {
                     queue.add(new int[]{i,j});
+                } else if(grid[i][j] == 1) {
+                    count_fresh++;
                 }
             }
         }
+
+        if(count_fresh == 0) return 0;
 
         int[][] dir = {{-1,0},{0,1},{0,-1},{1,0}};
 
@@ -29,6 +33,7 @@ class Solution {
                 flag=true;
             } else {
                 res++;
+                
             }
 
             for(int i=0; i<size; i++) {
@@ -47,19 +52,14 @@ class Solution {
                         grid[x][y]=2;
                         System.out.println(x + "and" + y);
                         queue.add(new int[]{x,y});
+                        count_fresh--;
                     }
                 }
             }
         }
 
-        for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
-                if(grid[i][j] == 1) {
-                    return -1;
-                }
-            }
-        }
+     
 
-      return res;  
+      return count_fresh == 0 ? res : -1;  
     }
 }
