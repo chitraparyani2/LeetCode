34718e6b43a770a55186a5bbc05251867cd0b614
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-04-15 13:56:45 -0400

    Time: 0 ms (100.00%) | Memory: 41.9 MB (82.94%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,22 +1,24 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         
-        int l1=m-1, l2=n-1;
+        int index = m+n-1;
 
-        int index = m+n;
+        while(m > 0 || n >0) {
 
-        while(l1 >= 0 || l2 >= 0) {
-            
-            if(l1 >= 0 && l2 >= 0 && nums1[l1] >= nums2[l2]) {
-                nums1[--index] = nums1[l1];
-                l1--;
-            } else if(l2 >= 0) {
-                nums1[--index] = nums2[l2];
-                l2--;
+            if(m > 0 && n > 0) {
+                if(nums1[m-1] >= nums2[n-1]) {
+                    nums1[index--] = nums1[m-1];
+                    m--;
+                } else {
+                    nums1[index--] = nums2[n-1];
+                    n--;
+                }
+            } else if(m== 0 && n > 0) {
+                nums1[index--] = nums2[n-1];
+                n--;
             } else {
-                l1--;
+                m--;
             }
         }
-
     }
 }
