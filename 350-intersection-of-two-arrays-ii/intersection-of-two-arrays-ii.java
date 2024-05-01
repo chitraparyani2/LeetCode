a80f2edf77c13ef390946efaef5f3dc94792abdb
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-05-01 15:11:12 -0400

    Time: 3 ms (90.33%) | Memory: 43 MB (70.65%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -1,32 +1,32 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
+        Map<Integer, Integer> map = new HashMap<>();
 
-        int i=0, j=0;
-        int n1=nums1.length, n2=nums2.length;
+        for(int i=0; i<nums1.length; i++) {
+            map.put(nums1[i], map.getOrDefault(nums1[i], 0)+1);
+        }
 
         List<Integer> res = new ArrayList<>();
 
-        while(i < n1 && j < n2) {
+        for(int i=0; i<nums2.length; i++) {
 
-            if(nums1[i] < nums2[j]) {
-                i++;
-            } else if(nums1[i] > nums2[j]) {
-                j++;
-            } else {
-                res.add(nums1[i]);
-                i++;
-                j++;
+            if(map.containsKey(nums2[i])) {
+                res.add(nums2[i]);
+                map.put(nums2[i], map.getOrDefault(nums2[i], 0)-1);
+
+                if(map.get(nums2[i]) == 0) {
+                    map.remove(nums2[i]);
+                }
+
             }
         }
 
         int[] result = new int[res.size()];
         int ind=0;
 
-        for(int num : res) {
-            result[ind++] = num;
+        for(int r : res) {
+            result[ind++] = r;
         }
 
       return result;  
