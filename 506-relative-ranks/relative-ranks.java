c90fdc63ceec4360803e5b77a816a17943705df7
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-05-08 12:43:04 -0400

    Time: 8 ms (80.99%) | Memory: 45.4 MB (73.42%) - LeetSync

--- 506-relative-ranks/relative-ranks.java
+++ 506-relative-ranks/relative-ranks.java
@@ -1,37 +1,32 @@
 class Solution {
     public String[] findRelativeRanks(int[] score) {
         
-        int n = score.length;
-        Integer[] res = new Integer[n];
-
-         for(int i=0; i<score.length; i++) {
-            res[i] = score[i];
-        }
-
-        Arrays.sort(res, Collections.reverseOrder());
-
-        Map<Integer, String> map = new HashMap<>();
-
-        for(int i=0; i<res.length; i++) {
-
-            if(i==0) {
-                map.put(res[i], "Gold Medal");
-            } else if(i == 1) {
-                map.put(res[i], "Silver Medal");
-            } else if(i == 2) {
-                map.put(res[i], "Bronze Medal");
-            } else {
-                map.put(res[i], i+1 + 
-                "");
-            }
-        }
-
-        String[] result = new String[n];
+        PriorityQueue<Integer> pq = 
+            new PriorityQueue<>((a,b) -> score[b] - score[a]);
 
         for(int i=0; i<score.length; i++) {
-            result[i] = map.get(score[i]);
+            pq.add(i);
         }
 
-      return result;  
+        String[] res = new String[score.length];
+        
+        if(!pq.isEmpty()) {
+            res[pq.poll()] = "Gold Medal";
+        }
+        if(!pq.isEmpty()) {
+            res[pq.poll()] = "Silver Medal";
+        }
+        if(!pq.isEmpty()) {
+            res[pq.poll()] = "Bronze Medal";
+        }
+
+        int place=4;
+
+        while(!pq.isEmpty()) {
+            res[pq.poll()] = place + "";
+            place++;
+        }
+
+      return res;  
     }
 }
