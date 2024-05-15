b22a137b1ad2d3df3b7435e38a4ead2f495b614c
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-05-15 13:18:39 -0400

    Time: 7 ms (18.76%) | Memory: 45.8 MB (82.55%) - LeetSync

--- 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.java
+++ 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.java
@@ -14,36 +14,58 @@
  * }
  */
 class Solution {
-    int sum = 0;
     public int sumEvenGrandparent(TreeNode root) {
         
-        if(root == null) {
-            return 0;
-        }
+        Queue<TreeNode> queue = new LinkedList();
+        int sum = 0;
+        queue.add(root);
 
-        if(root.val%2 == 0) {
-            if(root.left != null) {
-                if(root.left.left != null) {
-                    sum += root.left.left.val;
+        while(!queue.isEmpty()) {
+
+            int size = queue.size();
+
+            for(int i=0; i<size; i++) {
+
+                TreeNode temp = queue.poll();
+
+                if(temp.val % 2 == 0) {
+
+                    if(temp.left != null) {
+
+                        if(temp.left.left != null) {
+                            sum += temp.left.left.val;
+                        }
+
+                        if(temp.left.right != null) {
+                            sum += temp.left.right.val;
+                        }
+                       
+                    }
+
+                    if(temp.right != null) {
+
+                        if(temp.right.left != null) {
+                            sum += temp.right.left.val;
+                        }
+
+                        if(temp.right.right != null) {
+                            sum += temp.right.right.val;
+                        }
+                        
+                    }
+                   
                 }
-                if(root.left.right != null) {
-                    sum += root.left.right.val;
-                }
+                
+                    if(temp.left != null){
+                        queue.add(temp.left);
+                    }
+
+                    if(temp.right != null) {
+                        queue.add(temp.right);
+                    }
+
             }
-
-            if(root.right != null) {
-                if(root.right.left != null) {
-                    sum += root.right.left.val;
-                }
-                if(root.right.right != null) {
-                    sum += root.right.right.val;
-                }
-            }
         }
-
-        sumEvenGrandparent(root.left);
-        sumEvenGrandparent(root.right);
-
       return sum;  
     }
 }
