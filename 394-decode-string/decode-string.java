b0e97af549a564f0554d15ed586835405853f19b
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-05-14 22:12:52 -0400

    Time: 2 ms (38.55%) | Memory: 41.8 MB (16.06%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -1,60 +1,59 @@
 class Solution {
     public String decodeString(String s) {
         
-        Stack<Integer> stack1 = new Stack<>();
-        Stack<Character> stack2 = new Stack<>();
-        int num = 0;
-        StringBuilder res = new StringBuilder();
+        Stack<Integer> countStack = new Stack<>();
+        Stack<Character> charStack = new Stack<>();
+
+        int digit = 0;
         for(int i=0; i<s.length(); i++) {
 
             char ch = s.charAt(i);
 
             if(Character.isDigit(ch)) {
-                num = num * 10 + (ch - '0');
+                digit = digit * 10 + ch - '0';
             } else if(ch == '[') {
-                stack1.push(num);
-                num=0;
-                stack2.push(ch);
+                countStack.push(digit);
+                digit = 0;
+                charStack.push(ch);
             } else if(ch == ']') {
-                solve(stack1, stack2);
+                evaluate(countStack, charStack);
             } else {
-                stack2.push(ch);
+                charStack.push(ch);
             }
         }
 
-        while(!stack2.isEmpty()) {
-            res.append(stack2.pop());
+        StringBuilder res = new StringBuilder();
+
+        while(!charStack.isEmpty()) {
+            res.append(charStack.pop());
         }
+
       return res.reverse().toString();  
     }
 
-    public void solve(Stack<Integer> stack1, Stack<Character> stack2) {
+    public void evaluate(Stack<Integer> countStack, 
+                                Stack<Character> charStack) {
 
-        int val=stack1.pop();
-        StringBuilder sb = new StringBuilder();
+            
+            StringBuilder sb = new StringBuilder();
+            StringBuilder sb1 = new StringBuilder();
+            int count = countStack.pop();
 
-        while(!stack2.peek().equals('[')) {
-            sb.append(stack2.pop());
-        }
+            while(charStack.peek() != '[') {
+                sb.append(charStack.pop());
+            }
 
-        stack2.pop();
-        sb.reverse();
+            charStack.pop();
+            sb.reverse();
 
-        String res = evaluate(val, sb.toString());
+            for(int i=0; i<count; i++) {
+                sb1.append(sb.toString());
+            }
 
-        for(int i=0; i<res.length(); i++) {
-            char ch = res.charAt(i);
-            stack2.push(ch);
-        }
-    }
+            String s = sb1.toString();
 
-    public String evaluate(int val, String s) {
-
-        StringBuilder sb = new StringBuilder();
-
-        for(int i=0; i<val; i++) {
-            sb.append(s);
-        }
-      return sb.toString();  
+            for(int i=0; i<s.length(); i++) {
+                charStack.push(s.charAt(i));
+            }
     }
 }
