bca38b550655220b9854ec999d80a0cd433a08e7
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-05-15 14:15:49 -0400

    Time: 7 ms (13.78%) | Memory: 48.4 MB (98.08%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -8,26 +8,43 @@ class Solution {
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
                 if(grid[i][j] == '1') {
-                    helper(grid, i, j);
                     count++;
+
+                    grid[i][j] = '0';
+
+                    Queue<int[]> queue = new LinkedList<>();
+                    queue.add(new int[]{i,j});
+
+                    while(!queue.isEmpty()) {
+
+                        int[] temp = queue.poll();
+
+                        int r = temp[0];
+                        int c = temp[1];
+
+                        if(r-1 >= 0 && grid[r-1][c] == '1') {
+                            grid[r-1][c] = '0';
+                            queue.add(new int[]{r-1,c});
+                        }
+
+                        if(r+1 < grid.length && grid[r+1][c] == '1') {
+                            grid[r+1][c] = '0';
+                            queue.add(new int[]{r+1,c});
+                        }
+
+                        if(c-1 >= 0 && grid[r][c-1] == '1') {
+                            grid[r][c-1] = '0';
+                            queue.add(new int[]{r,c-1});
+                        }
+
+                        if(c+1 < grid[0].length && grid[r][c+1] == '1') {
+                            grid[r][c+1] = '0';
+                            queue.add(new int[]{r,c+1});
+                        }
+                    }
                 }
             }
         }
       return count;  
-    }
-
-    public void helper(char[][] grid, int r, int c) {
-
-        if(r < 0 || c < 0 || r>= grid.length || c >= grid[0].length ||
-        grid[r][c] == '0') {
-            return;
-        }
-
-        grid[r][c]='0';
-
-        helper(grid, r+1, c);
-        helper(grid, r-1, c);
-        helper(grid, r, c+1);
-        helper(grid, r, c-1);
     }
 }
