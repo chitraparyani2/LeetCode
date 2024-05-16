6eaea88267b16902eede1a88102d7c8254cc5b05
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-05-16 02:55:54 -0400

    Time: 0 ms (100.00%) | Memory: 41.6 MB (68.13%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -15,26 +15,37 @@ class Solution {
             return false;
         }
 
-        return helper(s1, s2, s3, s1.length()-1, s2.length()-1, s3.length()-1);
+        Boolean[][] memo = new Boolean[s1.length()+1][s2.length()+1];
+
+        return helper(s1, s2, s3, 0, 0, 0, memo);
     }
 
-    public boolean helper(String s1, String s2, String s3, int m, int n, int p) {
+    public boolean helper(String s1, String s2, String s3, int m, int n, int p,
+                            Boolean[][] memo) {
 
-        if(m < 0 && n < 0 && p < 0) {
+        if(m+n == s3.length()) {
             return true;
         }
 
-        if(m >=0  && s1.charAt(m) == s3.charAt(p)) {
-            if(helper(s1, s2, s3, m-1, n, p-1)) {
-                return true;
-            }
+        if(memo[m][n] != null) {
+            return memo[m][n];
         }
 
-        if(n >= 0 && s2.charAt(n) == s3.charAt(p)) {
-            if(helper(s1, s2, s3, m, n-1, p-1)) {
-                return true;
-            }
+        if(m < s1.length() && s1.charAt(m) == s3.charAt(p)) {
+                boolean flag = helper(s1, s2, s3, m+1, n, p+1, memo);
+                memo[m][n] = flag;
+                if(flag)
+                    return true;
         }
+
+        if(n < s2.length() && s2.charAt(n) == s3.charAt(p)) {
+                boolean flag = helper(s1, s2, s3, m, n+1, p+1, memo);
+                memo[m][n] = flag;
+                if(flag)
+                    return true;
+        }
+
+        memo[m][n] = false;
       return false;  
     }
 }
