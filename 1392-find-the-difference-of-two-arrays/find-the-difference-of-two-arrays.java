06a67cfbdc99df568277472835871e4389304c88
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-06-11 10:19:43 -0400

    Time: 62 ms (14.83%) | Memory: 45.2 MB (66.00%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
@@ -5,25 +5,30 @@ class Solution {
         List<Integer> list1 = new ArrayList();
         List<Integer> list2 = new ArrayList();
 
-        Map<Integer, Integer> map1 = new HashMap();
-        Map<Integer, Integer> map2 = new HashMap();
+        // Map<Integer, Integer> map1 = new HashMap();
+        // Map<Integer, Integer> map2 = new HashMap();
+
+        Set<Integer> set1 = new HashSet();
+        Set<Integer> set2 = new HashSet();
 
         for(int num : nums1) {
-            map1.put(num, map1.getOrDefault(num,0)+1);
+            // map1.put(num, map1.getOrDefault(num,0)+1);
+            set1.add(num);
         }
 
         for(int num : nums2) {
-            map2.put(num, map2.getOrDefault(num,0)+1);
+           // map2.put(num, map2.getOrDefault(num,0)+1);
+           set2.add(num);
         }
 
         for(int num : nums1) {
-            if(!map2.containsKey(num) && !list1.contains(num)) {
+            if(!set2.contains(num) && !list1.contains(num)) {
                 list1.add(num);
             }
         }
 
         for(int num : nums2) {
-            if(!map1.containsKey(num) && !list2.contains(num)) {
+            if(!set1.contains(num) && !list2.contains(num)) {
                 list2.add(num);
             }
         }
