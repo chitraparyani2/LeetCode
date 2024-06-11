86fbe3c73941a636c12d6f4f9ec1152982ddcc9f
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-06-11 10:15:03 -0400

    Time: 63 ms (13.33%) | Memory: 45.5 MB (15.91%) - LeetSync

---
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        
+        List<List<Integer>> res = new ArrayList();
+        List<Integer> list1 = new ArrayList();
+        List<Integer> list2 = new ArrayList();
+
+        Map<Integer, Integer> map1 = new HashMap();
+        Map<Integer, Integer> map2 = new HashMap();
+
+        for(int num : nums1) {
+            map1.put(num, map1.getOrDefault(num,0)+1);
+        }
+
+        for(int num : nums2) {
+            map2.put(num, map2.getOrDefault(num,0)+1);
+        }
+
+        for(int num : nums1) {
+            if(!map2.containsKey(num) && !list1.contains(num)) {
+                list1.add(num);
+            }
+        }
+
+        for(int num : nums2) {
+            if(!map1.containsKey(num) && !list2.contains(num)) {
+                list2.add(num);
+            }
+        }
+
+        res.add(list1);
+        res.add(list2);
+
+      return res;  
+    }
+}
