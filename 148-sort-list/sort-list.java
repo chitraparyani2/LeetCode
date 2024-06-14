536965f536f95288e36e5d0a7a90dd473c43f8a9
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-06-14 11:03:02 -0400

    Time: 14 ms (21.29%) | Memory: 58.4 MB (5.11%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -36,29 +36,16 @@ class Solution {
 
     public ListNode merge(ListNode list1, ListNode list2) {
 
-        ListNode res = new ListNode(-1);
-        ListNode curr = res;
-
-        while(list1 != null && list2 != null) {
-
-            if(list1.val <= list2.val) {
-                curr.next = list1;
-                list1 = list1.next;
-            } else {
-                curr.next = list2;
-                list2 = list2.next;
-            }
-            curr = curr.next;
-        }
-
-        if(list1 != null) {
-            curr.next = list1;
-        }
-
-        if(list2 != null) {
-            curr.next = list2;
-        }
-
-      return res.next;  
+     if(list1 == null) {
+        return list2;
+     } else if(list2 == null) {
+        return list1;
+     } else if(list1.val <= list2.val) {
+        list1.next = merge(list1.next, list2);
+        return list1;
+     } else {
+        list2.next = merge(list1, list2.next);
+        return list2;
+     }
     }
 }
