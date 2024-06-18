09029e78839c22293041987cc898fedc9739671f
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-06-18 15:32:34 -0400

    Time: 17 ms (56.17%) | Memory: 44.7 MB (59.22%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,77 +1,53 @@
 class Solution {
-    /*
-
-    --
-            l, r
-        S -> ADOBECODEBANC (N2* N -> N3)
-        T -> ABC
-
-        Map -> charcter, count
-
-        Everything becomes 0 -> 
-        counter -> once value becomes 0
-
-        AABC
-
-        A ,2
-
-    */
     public String minWindow(String s, String t) {
         
-        Map<Character, Integer> countMap = new HashMap<>();
-        for(Character ch : t.toCharArray()){
-            countMap.put(ch, countMap.getOrDefault(ch, 0) + 1);
+        Map<Character, Integer> map = new HashMap<>();
+
+        for(int i=0; i<t.length(); i++) {
+            char ch = t.charAt(i);
+
+            map.put(ch, map.getOrDefault(ch,0)+1);
         }
 
-        int count = countMap.size(); // no of unique characters in target string
+        int st=0, ed=0, count=0, lt=0, rt=0, res=Integer.MAX_VALUE;
 
-        int st = 0, e = 0;
+        while(ed < s.length()) {
 
-        int counter = 0;
+            char ch = s.charAt(ed);
 
-        int len = Integer.MAX_VALUE, si = -1, ei = -1;
-        // left, right
-        // 1. while -> (right < length of String)
-        // 2. take character from right pointer
-        // 3. Do app operation on it
-        // 4. Check the condtion where you need to move left pointer
-        // 5. while -> (left <= right)
-        // 6. now take the left char and reset/reduce the appropriate operation
-        // 7. when to break and start pointer ++
-        // 8. Outside second while -> increment right pointer
-        while(e < s.length()){
-
-            char ch = s.charAt(e);
-
-            if(countMap.containsKey(ch)){
-                countMap.put(ch, countMap.get(ch) - 1);
-                if(countMap.get(ch) == 0){
-                    counter++;
+            if(map.containsKey(ch)) {
+                map.put(ch, map.get(ch)-1);
+                if(map.get(ch) == 0) {
+                    count++;
                 }
             }
 
+            while(st <= ed && count == map.size()) {
 
-                while(st <= e && counter == count){
-                    if(len > e - st + 1){
-                        len = e - st + 1;
-                        si = st;
-                        ei = e;
-                    }
-                    char chSt = s.charAt(st);
-                    if(countMap.containsKey(chSt)){
-                        countMap.put(chSt, countMap.get(chSt) + 1);
-                        if(countMap.get(chSt) == 1){
-                            counter--;  
-                        }
-                    }
-                    st++;
+                int len = ed-st+1;
+
+                if(res > len) {
+                    res = len;
+                    rt = ed;
+                    lt = st;
                 }
 
-            e++;
+                char ch1 = s.charAt(st);
 
+                if(map.containsKey(ch1)) {
+
+                    map.put(ch1, map.get(ch1)+1);
+
+                    if(map.get(ch1) == 1) {
+                        count--;
+                    }
+                }
+                st++;
+            }
+
+          ed++;  
         }
 
-        return si == -1?"":s.substring(si, ei + 1);
-
+      return res == Integer.MAX_VALUE ? "" : s.substring(lt, rt+1);  
     }
 }
