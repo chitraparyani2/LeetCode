519d0c67db55c2c8827ab10400c73708d2fcc6f0
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-06-19 12:05:36 -0400

    Time: 164 ms (16.75%) | Memory: 45.3 MB (99.07%) - LeetSync

--- 769-largest-plus-sign/largest-plus-sign.java
+++ 769-largest-plus-sign/largest-plus-sign.java
@@ -2,46 +2,40 @@ class Solution {
     public int orderOfLargestPlusSign(int n, int[][] mines) {
         
         int[][] grid = new int[n][n];
-        int ans = 0;
-
+        int res = 0;
         for(int[] g : grid) {
             Arrays.fill(g, 1);
         }
 
-        for(int[] mine : mines) {
-            int x = mine[0];
-            int y = mine[1];
+        for(int[] m : mines) {
+            int x = m[0];
+            int y = m[1];
 
             grid[x][y] = 0;
         }
 
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
+
                 if(grid[i][j] == 1) {
-                    int res = helper(grid, i, j);
-                    ans = Math.max(ans, res);
+
+                    int max = 1;
+                    int s = 1;
+
+                    while(i-s >= 0 && i+s < n && j-s >= 0 && j+s < n &&
+                        grid[i-s][j] == 1 && grid[i+s][j] == 1 &&
+                        grid[i][j-s] == 1 && grid[i][j+s] == 1)  {
+
+                            max++;
+                            s++;
+
+                    }
+
+                    res = Math.max(res, max);
                 }
             }
         }
-      return ans;  
-    }
 
-    public int helper(int[][] grid, int r, int c) {
-
-        int s = 1;
-        int ans = 1;
-        while(r-s >= 0 && r+s < grid.length &&
-                c-s >= 0 && c+s < grid[0].length) {
-
-                    if(grid[r-s][c] == 1 && grid[r+s][c] == 1 &&
-                        grid[r][c-s] == 1 && grid[r][c+s] == 1) {
-                            ans++;
-                            s++;
-                        } else {
-                            break;
-                        }
-                }
-
-      return ans;          
+      return res;  
     }
 }
