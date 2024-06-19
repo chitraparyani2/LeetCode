50abf5476e81c52278a6938304eccf8835db074a
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-06-19 00:04:40 -0400

    Time: 2 ms (87.98%) | Memory: 56 MB (28.16%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,44 +1,27 @@
 class Solution {
-
-    /**
-        [1, 1, 2, 6]
-
-        [24,12,4, 1]
-
-        [1, 2, 3, 4]
-
-        [24, 12, 8, 6]
-       
-     */
-
     public int[] productExceptSelf(int[] nums) {
         
         int n = nums.length;
 
-        int[] l = new int[n];
-       // int[] r = new int[n];
-
-        int right = 1;
+        int[] left = new int[n];
+        int[] right = new int[n];
         int[] res = new int[n];
 
-        l[0] = 1;
-       // r[n-1] = 1;
+        left[0] = 1;
+        right[n-1] = 1;
 
         for(int i=1; i<n; i++) {
-            
-            l[i] = nums[i-1] * l[i-1];
+            left[i] = left[i-1] * nums[i-1];
         }
 
-        // for(int i=n-2; i>=0; i--) {
-        //     r[i] = nums[i+1] * r[i+1];
-        // }
-
-        for(int i=n-2; i>=0; i--) {
-           // res[i] = l[i] * r[i];
-           right = right * nums[i+1];
-           l[i] = l[i] * right;
+        for(int j=n-2; j>=0; j--) {
+            right[j] = right[j+1] * nums[j+1];
         }
 
-      return l;  
+        for(int k=0; k<n; k++) {
+            res[k] = left[k] * right[k];
+        }
+
+      return res;  
     }
 }
