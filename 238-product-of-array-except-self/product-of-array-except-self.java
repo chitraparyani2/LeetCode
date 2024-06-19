310ed764b975cc27d476ac29a2db848320f96151
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-06-19 00:07:15 -0400

    Time: 2 ms (87.98%) | Memory: 55.2 MB (59.33%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -4,24 +4,25 @@ class Solution {
         int n = nums.length;
 
         int[] left = new int[n];
-        int[] right = new int[n];
-        int[] res = new int[n];
-
+        //int[] right = new int[n];
+       // int[] res = new int[n];
+        
         left[0] = 1;
-        right[n-1] = 1;
+       // right[n-1] = 1;
+
+       int right = 1;
 
         for(int i=1; i<n; i++) {
             left[i] = left[i-1] * nums[i-1];
         }
 
         for(int j=n-2; j>=0; j--) {
-            right[j] = right[j+1] * nums[j+1];
+            right = right * nums[j+1];
+            left[j] = left[j] * right;
         }
 
-        for(int k=0; k<n; k++) {
-            res[k] = left[k] * right[k];
-        }
+    
 
-      return res;  
+      return left;  
     }
 }
