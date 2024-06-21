db483182d89b3e08d02e2588b77c82eb8736f8d9
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-06-21 14:59:24 -0400

    Time: 4 ms (55.66%) | Memory: 41.5 MB (67.01%) - LeetSync

--- 778-reorganize-string/reorganize-string.java
+++ 778-reorganize-string/reorganize-string.java
@@ -1,58 +1,51 @@
 class Solution {
     public String reorganizeString(String s) {
-        
+
         Map<Character, Integer> map = new HashMap<>();
 
-        char maxch='-';
-        int max=0;
-        int n=s.length();
-
-        char[] res = new char[n];
+        int n = s.length();
+        char maxChar = '_';
+        int maxFreq = 0;
 
         for(int i=0; i<s.length(); i++) {
-
             char ch = s.charAt(i);
             map.put(ch, map.getOrDefault(ch,0)+1);
-
-            if(map.get(ch) > max) {
-                max = map.get(ch);
-                maxch = ch;
+            if(map.get(ch) > maxFreq) {
+                maxFreq = map.get(ch);
+                maxChar = ch;
             }
         }
 
-        if(max > (n+1)/2) {
+        if(maxFreq > (n+1)/2) {
             return "";
         }
 
-        int index=0;
+        int index = 0;
+        char[] arr = new char[n];
 
-        for(int i=0; i<max; i++) {
-            res[index] = maxch;
+        for(int i=0; i<maxFreq; i++) {
+            arr[index] = maxChar;
             index+=2;
         }
 
-        map.remove(maxch);
+        map.remove(maxChar);
 
         for(int i=0; i<s.length(); i++) {
-
-            char c = s.charAt(i);
-
-            if(index >= n) {
+            if(index>= n) {
                 index=1;
             }
-
-            int val=map.getOrDefault(c,0);
+            char ch = s.charAt(i);
+            int val = map.getOrDefault(ch,0);
 
             for(int j=0; j<val; j++) {
-                if(index >= n) {
+                if(index>=n) {
                     index=1;
                 }
-                res[index] = c;
+                arr[index] = ch;
                 index+=2;
             }
-            map.remove(c);
+            map.remove(ch);
         }
-
-      return new String(res);  
+       return new String(arr);
     }
 }
