21f7157b5def445cec9223b3467e10bfed61c19a
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-07-07 12:39:22 -0400

    Time: 9 ms (11.52%) | Memory: 49.3 MB (53.75%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,42 +1,94 @@
 class Solution {
-    public int numIslands(char[][] grid) {
-        
-        int m = grid.length;
-        int n = grid[0].length;
+
+    class UnionFind {
+
+        int[] parent;
+        int[] rank;
         int count = 0;
 
-        Queue<int[]> queue = new LinkedList();
+        public UnionFind(char[][] grid) {
 
-        int[][] dir = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+            int m = grid.length;
+            int n = grid[0].length;
 
-        for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
-                if(grid[i][j] == '1') {
-                    grid[i][j] = '0';
-                    queue.add(new int[]{i,j});
+            parent = new int[m*n];
+            rank = new int[m*n];
 
-                    while(!queue.isEmpty()) {
+            for(int i=0; i<m; i++) {
+                for(int j=0; j<n; j++) {
 
-                        int[] temp = queue.poll();
-
-                        for(int[] d : dir) {
-
-                            int x = temp[0] + d[0];
-                            int y = temp[1] + d[1];
-
-                            if(x < 0 || x >= m || y < 0 || y >= n || grid[x][y] == '0') {
-                                continue;
-                            }
-
-                            grid[x][y] = '0';
-
-                            queue.add(new int[]{x,y});
-                        }
+                    if(grid[i][j] == '1') {
+                        parent[i*n+j] = i*n+j;
+                        count++;
+                        rank[i*n+j] = 0;
                     }
-                    count++;
                 }
             }
         }
-       return count; 
+
+        public int findParent(int x) {
+
+            if(x != parent[x]) {
+                return findParent(parent[x]);
+            }
+          return parent[x];  
+        }
+
+        public void union(int x, int y) {
+
+            int rootX = findParent(x);
+            int rootY = findParent(y);
+
+            if(rootX != rootY) {
+
+                if(rank[rootX] >= rank[rootY]) {
+                    rank[rootX]++;
+                    parent[rootY] = rootX;
+                } else {
+                    rank[rootY]++;
+                    parent[rootX] = rootY;
+                }
+                count--; 
+            }
+           
+        }
+
+        public int getCount() {
+            return count;
+        }
+    }
+
+    public int numIslands(char[][] grid) {
+        
+        UnionFind uf = new UnionFind(grid);
+
+        int m = grid.length;
+        int n = grid[0].length;
+
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+
+                if(grid[i][j] == '1') {
+                    grid[i][j] = '0';
+                    if(i-1 >= 0 && grid[i-1][j] == '1') {
+                        uf.union(i*n+j, (i-1)*n+j);
+                    }
+
+                    if(i+1 < m && grid[i+1][j] == '1') {
+                        uf.union(i*n+j, (i+1)*n+j);
+                    }
+
+                    if(j-1 >= 0 && grid[i][j-1] == '1') {
+                        uf.union(i*n+j, i*n+j-1);
+                    }
+
+                    if(j+1 < n && grid[i][j+1] == '1') {
+                        uf.union(i*n+j, i*n+j+1);
+                    }
+                }
+            }
+        }
+
+      return uf.count;  
     }
 }
