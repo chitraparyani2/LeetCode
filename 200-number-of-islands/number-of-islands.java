b7e61495480f44e52096658fd24955e8300d3e36
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-07-06 21:01:43 -0400

    Time: 7 ms (18.85%) | Memory: 48.2 MB (99.59%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -5,30 +5,38 @@ class Solution {
         int n = grid[0].length;
         int count = 0;
 
+        Queue<int[]> queue = new LinkedList();
+
+        int[][] dir = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
                 if(grid[i][j] == '1') {
-                    helper(grid, i, j);
+                    grid[i][j] = '0';
+                    queue.add(new int[]{i,j});
+
+                    while(!queue.isEmpty()) {
+
+                        int[] temp = queue.poll();
+
+                        for(int[] d : dir) {
+
+                            int x = temp[0] + d[0];
+                            int y = temp[1] + d[1];
+
+                            if(x < 0 || x >= m || y < 0 || y >= n || grid[x][y] == '0') {
+                                continue;
+                            }
+
+                            grid[x][y] = '0';
+
+                            queue.add(new int[]{x,y});
+                        }
+                    }
                     count++;
                 }
             }
         }
-      return count;  
-    }
-
-    public void helper(char[][] grid, int r, int c) {
-
-        if(r < 0 || r >= grid.length || c < 0 ||
-            c >= grid[0].length || grid[r][c] == '0') {
-                return;
-            }
-
-            grid[r][c] = '0';
-
-            helper(grid, r-1, c);
-            helper(grid, r+1, c);
-            helper(grid, r, c-1);
-            helper(grid, r, c+1);
-
+       return count; 
     }
 }
