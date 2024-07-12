6bb703c818268313513dbbc3ba05b1eaeb596a45
chitraparyani2 <144478506+chitraparyani2@users.noreply.github.com>
2024-07-11 20:42:52 -0400

    Time: 9 ms (93.57%) | Memory: 57.2 MB (21.33%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -15,37 +15,51 @@ class Solution {
             return head;
         }
 
+        ListNode prev = head;
         ListNode slow = head;
         ListNode fast = head;
-        ListNode prev = slow;
 
         while(fast != null && fast.next != null) {
-            
             prev = slow;
             slow = slow.next;
             fast = fast.next.next;
         }
 
         prev.next = null;
-        ListNode list1 = sortList(head);
-        ListNode list2 = sortList(slow);
 
-        return merge(list1, list2);
+        ListNode list1 = sortList(slow);
+        ListNode list2 = sortList(head);
 
+        ListNode res = merge(list1, list2);
+        
+      return res; 
     }
 
     public ListNode merge(ListNode list1, ListNode list2) {
 
-     if(list1 == null) {
-        return list2;
-     } else if(list2 == null) {
-        return list1;
-     } else if(list1.val <= list2.val) {
-        list1.next = merge(list1.next, list2);
-        return list1;
-     } else {
-        list2.next = merge(list1, list2.next);
-        return list2;
-     }
+        ListNode res = new ListNode(-1);
+        ListNode curr = res;
+
+        while(list1 != null && list2 != null) {
+
+            if(list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+                curr = curr.next;
+        }
+
+        if(list1 != null) {
+            curr.next = list1;
+        }
+
+        if(list2 != null) {
+            curr.next = list2;
+        }
+
+       return res.next; 
     }
 }
